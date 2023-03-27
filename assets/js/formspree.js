b532086ed377eefc4d64c1360530ab1b93a148ef
jcourbage <jcourbage@murex.com>
2023-03-27 11:43:14 +0200

    Fix contact form

--- assets/js/formspree.js
+++ assets/js/formspree.js
@@ -1,21 +1,30 @@
-var $contactForm = $('#contact-form');
-$contactForm.submit(function(e) {
-	e.preventDefault();
-	$.ajax({
-		url: '//formspree.io/contact@cartescolaire.paris',
-		method: 'POST',
-		data: $(this).serialize(),
-		dataType: 'json',
-		beforeSend: function() {
-			$contactForm.append('<div class="alert alert--loading">Envoi …</div>');
-		},
-		success: function(data) {
-			$('#aboutModal').modal('hide');
-			alert("Merci !");
-		},
-		error: function(err) {
-			$contactForm.find('.alert--loading').hide();
-			$contactForm.append('<div class="alert alert--error">Erreur :-(</div>');
+var form = document.getElementById("contact-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+	method: form.method,
+	body: data,
+	headers: {
+		'Accept': 'application/json'
+	}
+  }).then(response => {
+	if (response.ok) {
+	  $('#aboutModal').modal('hide');
+	  alert("Merci !");
+	  form.reset()
+	} else {
+	  response.json().then(data => {
+		if (Object.hasOwn(data, 'errors')) {
+		  status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+		} else {
+			form.append('<div class="alert alert--error">Erreur :-(</div>')
 		}
-	});
-});
+	  })
+	}
+  }).catch(error => {
+	form.append('<div class="alert alert--error">Erreur :-(</div>')
+  });
+}
+form.addEventListener("submit", handleSubmit)
